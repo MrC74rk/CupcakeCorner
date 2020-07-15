1dc00d5d02984b9c05b42c05255cced1b949a605
Bryan Angelo <45373566+MrC74rk@users.noreply.github.com>
2020-07-15 15:24:30 -0500

    Add files via upload

---
+++ CupcakeCorner/CupcakeCorner/SnipitView.swift
@@ -0,0 +1,21 @@
+//
+//  SnipitView.swift
+//  CupcakeCorner
+//
+//  Created by Bryan Angelo on 7/15/20.
+//  Copyright © 2020 Bryan Angelo. All rights reserved.
+//
+
+import SwiftUI
+
+struct SnipitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SnipitView_Previews: PreviewProvider {
+    static var previews: some View {
+        SnipitView()
+    }
+}
